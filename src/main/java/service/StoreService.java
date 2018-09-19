ac6a843a547b1e1c00e446243c8755dbb9d92b21
Anatoly Dyachenko <oreh17@gmail.com>
2018-09-19 22:53:37 +0300

    Initial commit.

---
+++ src/main/java/service/StoreService.java
@@ -0,0 +1,18 @@
+package service;
+
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.lambdaworks.crypto.SCryptUtil;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+
+//@Path("/")
+public class StoreService {
+
+}
